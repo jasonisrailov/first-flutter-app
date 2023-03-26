fc1b581c102aea8a4a85dde401c8ca8e580a0357
Jason Israilov <jasonisrailov@gmail.com>
2023-03-26 11:42:27 -0400

    addings favorites page

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # first_flutter_app
 
-This is my first Flutter app from the _Google Developer Groups: Flutter Forward_ event at Google in Ann Arbor.
+This is my first Flutter app from the _Google Developer Groups: Flutter Forward_ event at Google in Ann Arbor. This project is a starting point for a Flutter application.
 
-## Getting Started
+Below is a list of all the amazing things I learned from this GDG event and the Codelab:
+- The basics of how Flutter works
+- Creating layouts in Flutter
+- Connecting user interactions (like button presses) to app behavior
+- Keeping your Flutter code organized
+- Making your app responsive
+- Achieving a consistent look & feel of your app
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+A few resources I used:
+- [Lab: Your first Flutter app](https://codelabs.developers.google.com/codelabs/flutter-codelab-first#0)
+- [Flutter online documentation](https://docs.flutter.dev/)
--- lib/main.dart
+++ lib/main.dart
@@ -64,45 +64,48 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
-                ),
-              ],
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedIndex = value;
-                });
-              },
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
             ),
-          ),
-          Expanded(
-            // This asks the widget for as much space as possible
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
+            Expanded(
+              // This asks the widget for as much space as possible
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
 
@@ -177,6 +180,35 @@ class BigCard extends StatelessWidget {
               "${pair.first} ${pair.second}", // this helps the screenreader read both words correctly
         ),
       ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      // ListView is a column that you can scroll
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
